5c5993475377a5c82161ec465d632cd10c9e1736
riccardo <giordanobruno227@gmail.com>
2024-03-13 12:29:46 +0100

    added stuffs

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var csso = require('gulp-csso');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var sass = require('gulp-sass');
+var sass = require('gulp-sass')(require('sass'));
 var plumber = require('gulp-plumber');
 var cp = require('child_process');
 var imagemin = require('gulp-imagemin');
@@ -14,33 +14,33 @@ var jekyllCommand = (/^win/.test(process.platform)) ? 'jekyll.bat' : 'bundle';
  * Build the Jekyll Site
  * runs a child process in node that runs the jekyll commands
  */
-gulp.task('jekyll-build',['sass'], function (done) {
+gulp.task('jekyll-build', function (done) {
 	return cp.spawn(jekyllCommand, ['exec', 'jekyll', 'build'], {stdio: 'inherit'}).on('close', done);
 });
 
 /*
  * Rebuild Jekyll & reload browserSync
  */
-gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+gulp.task('jekyll-rebuild',  gulp.series('jekyll-build', function () {
 	browserSync.reload();
-});
+}));
 
 /*
  * Build the jekyll site and launch browser-sync
  */
-gulp.task('browser-sync', ['jekyll-build'], function() {
+gulp.task('browser-sync',  gulp.series('jekyll-build', function() {
 	browserSync({
 		server: {
 			baseDir: '_site'
 		}
 	});
-});
+}));
 
 /*
 * Compile and minify sass
 */
 gulp.task('sass', function() {
-  	gulp.src('src/styles/**/*.scss')
+  	 return gulp.src('src/styles/**/*.scss')
 		.pipe(plumber())
 		.pipe(sass())
 		.pipe(csso())
@@ -51,7 +51,7 @@ gulp.task('sass', function() {
 * Compile fonts
 */
 gulp.task('fonts', function() {
-	gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
+	return gulp.src('src/fonts/**/*.{ttf,woff,woff2}')
 		.pipe(plumber())
 		.pipe(gulp.dest('assets/fonts/'));
 })
@@ -85,4 +85,4 @@ gulp.task('watch', function() {
 	gulp.watch(['*html', '_includes/*html', '_layouts/*.html'], ['jekyll-rebuild']);
 });
 
-gulp.task('default', ['js', 'sass', 'fonts', 'browser-sync', 'watch']);
+gulp.task('default',  gulp.series('js', 'sass', 'fonts', 'browser-sync', 'watch'));
